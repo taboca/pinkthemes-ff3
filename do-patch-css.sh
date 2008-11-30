392fc1199b9da855bc31e7e46e203d1a112d87a4
root <taboca@gmail.com>
2008-11-29 22:28:59 -0200

    More graphics css etc

--- do-patch-css.sh
+++ do-patch-css.sh
@@ -2,4 +2,4 @@
 rm -f patch-tmp/*
 
 ./patch-include/filestrip.sh ./src/theme/svg/biblioteca/Toolbar/buttons-integrated.svg > ./patch-tmp/buttons-integrated.svg
-cat ./patch-include/xmlprelude.txt "<?xml-stylesheet href=\"../base.css\" type=\"text/css\"?>" ./patch-tmp/buttons-integrated.svg > ./src/theme/svg/biblioteca/Toolbar/buttons-integrated.svg
+cat ./patch-include/xmlprelude.txt ./patch-include/cssToolbar.txt  ./patch-tmp/buttons-integrated.svg > ./src/theme/svg/biblioteca/Toolbar/buttons-integrated.svg
