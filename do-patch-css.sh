141031fab1fe694eb2978b22f833796232a501f3
root <taboca@gmail.com>
2008-12-22 19:32:21 -0200

    Lots of changes, polish to files, removed old files etc

--- do-patch-css.sh
+++ do-patch-css.sh
@@ -3,3 +3,5 @@ rm -f patch-tmp/*
 
 ./patch-include/filestrip.sh ./src/theme/svg/biblioteca/Toolbar/buttons-integrated.svg > ./patch-tmp/buttons-integrated.svg
 cat ./patch-include/xmlprelude.txt ./patch-include/cssToolbar.txt  ./patch-tmp/buttons-integrated.svg > ./src/theme/svg/biblioteca/Toolbar/buttons-integrated.svg
+       
+# ./src/theme/svg/biblioteca/Global/icons/tab-complete.svg needs to import ../../base.css 
