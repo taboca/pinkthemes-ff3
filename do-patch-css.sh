fd4202b30ec6573ce83448a7c4ff6d3ab1b896ca
root <taboca@gmail.com>
2008-12-23 02:21:56 -0200

    Lots of fixes for tab area

--- do-patch-css.sh
+++ do-patch-css.sh
@@ -5,3 +5,5 @@ rm -f patch-tmp/*
 cat ./patch-include/xmlprelude.txt ./patch-include/cssToolbar.txt  ./patch-tmp/buttons-integrated.svg > ./src/theme/svg/biblioteca/Toolbar/buttons-integrated.svg
        
 # ./src/theme/svg/biblioteca/Global/icons/tab-complete.svg needs to import ../../base.css 
+#  src/theme/svg/src/global/popup-bg.svg has ../../base.css
+# vi src/theme/svg/src/global/toolbar/toolbar-background.svg  ../../../base.css
