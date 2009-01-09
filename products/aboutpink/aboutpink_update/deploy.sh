87da6af44f14c9993259e203d2bb911b84484eee
Marcio Galli <taboca@gmail.com>
2009-01-08 21:05:32 -0300

    0 5 fix and install fix

--- products/aboutpink/aboutpink_update/deploy.sh
+++ products/aboutpink/aboutpink_update/deploy.sh
@@ -1,3 +1,3 @@
 scp aboutpink_update.rdf tab@www.taboca.com:/home/tab
-scp ../release-latest/aboutpink.xpi tab@www.taboca.com:/home/tab/aboutpinkv04.xpi
+scp ../release-latest/aboutpink.xpi tab@www.taboca.com:/home/tab/aboutpinkv05.xpi
 
