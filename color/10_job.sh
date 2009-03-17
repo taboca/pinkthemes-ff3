5faebfdbcb4cb07644bac1f95b2d02294330df05
root <taboca@gmail.com>
2009-03-17 01:25:28 -0300

    LL

--- color/10_job.sh
+++ color/10_job.sh
@@ -2,11 +2,11 @@
 
 cp fixcss.sh replace.sh
 
-./_colorhigh.py $1 $2 $3 $4 
-./_colorlow.py $1 $2 $3 $5 
+./colorhigh.py $1 $2 $3 $4 
+./colorlow.py $1 $2 $3 $5 
 
-./_sedcolorhigh.sh 
-./_sedcolorlow.sh 
+./sedcolorhigh.sh 
+./sedcolorlow.sh 
 
 
 mkdir base-$1-$2-$3
@@ -14,8 +14,8 @@ mkdir base-$1-$2-$3
 cp replace.sh base-$1-$2-$3/fixcss.sh
 
 cp base.css replace.sh
-./_sedcolorhigh.sh 
-./_sedcolorlow.sh 
+./sedcolorhigh.sh 
+./sedcolorlow.sh 
 
 cp replace.sh base-$1-$2-$3/base.css
 
