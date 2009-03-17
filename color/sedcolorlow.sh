8fdb3a0b66f1ca581a408d34d71a020f1d2f1798
root <taboca@gmail.com>
2009-03-17 01:18:39 -0300

    LovelyLilac

--- color/sedcolorlow.sh
+++ color/sedcolorlow.sh
@@ -1,16 +1,16 @@
-sed -e "s/colorlownormal/rgb(200,229,252)/" ./replace.sh > temp.css
+sed -e "s/colorlownormal/rgb(223,190,255)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow1/rgb(180,207,227)/" ./replace.sh > temp.css
+sed -e "s/colorlow1/rgb(201,171,230)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow3/rgb(140,163,177)/" ./replace.sh > temp.css
+sed -e "s/colorlow3/rgb(157,133,180)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow2/rgb(160,185,202)/" ./replace.sh > temp.css
+sed -e "s/colorlow2/rgb(179,152,205)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow5/rgb(100,119,127)/" ./replace.sh > temp.css
+sed -e "s/colorlow5/rgb(113,95,130)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow4/rgb(120,141,152)/" ./replace.sh > temp.css
+sed -e "s/colorlow4/rgb(135,114,155)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow7/rgb(60,75,77)/" ./replace.sh > temp.css
+sed -e "s/colorlow7/rgb(69,57,80)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow6/rgb(80,97,102)/" ./replace.sh > temp.css
+sed -e "s/colorlow6/rgb(91,76,105)/" ./replace.sh > temp.css
 cp temp.css replace.sh
