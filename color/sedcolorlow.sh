8b5d5096de8e35507b16c01c550c4e751449f7fd
root <taboca@gmail.com>
2009-07-20 22:00:40 -0300

    iFix to LLx!

--- color/sedcolorlow.sh
+++ color/sedcolorlow.sh
@@ -1,16 +1,16 @@
-sed -e "s/colorlownormal/rgb(250,190,240)/" ./replace.sh > temp.css
+sed -e "s/colorlownormal/rgb(220,190,255)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow1/rgb(225,171,216)/" ./replace.sh > temp.css
+sed -e "s/colorlow1/rgb(198,171,230)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow3/rgb(175,133,168)/" ./replace.sh > temp.css
+sed -e "s/colorlow3/rgb(154,133,180)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow2/rgb(200,152,192)/" ./replace.sh > temp.css
+sed -e "s/colorlow2/rgb(176,152,205)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow5/rgb(125,95,120)/" ./replace.sh > temp.css
+sed -e "s/colorlow5/rgb(110,95,130)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow4/rgb(150,114,144)/" ./replace.sh > temp.css
+sed -e "s/colorlow4/rgb(132,114,155)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow7/rgb(75,57,72)/" ./replace.sh > temp.css
+sed -e "s/colorlow7/rgb(66,57,80)/" ./replace.sh > temp.css
 cp temp.css replace.sh
-sed -e "s/colorlow6/rgb(100,76,96)/" ./replace.sh > temp.css
+sed -e "s/colorlow6/rgb(88,76,105)/" ./replace.sh > temp.css
 cp temp.css replace.sh
