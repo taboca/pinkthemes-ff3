2b08247e1103bf48e09d1f3362e4d092e67e58ee
Marcio Galli <marciogalli@taboca.local>
2009-09-01 14:42:56 -0300

    fix to abouttimao namespace

---
+++ products/abouttimao/src-backup/chrome/content/abouttimao.js
@@ -0,0 +1,280 @@
+////
+//// About Timao supports Timao Paula in the Timao Paula Pack edition. This brings the about:timao experience where the end-user 
+//// can customize their timao experience to something beyond the basic theme. At this point the about:timao offers
+
+// Ability to add the TimaoTheme Timao custom Search 
+// Release notes 
+// Link to support pages
+// And more
+// Copyright (C) 2009 Taboca Inc 
+
+/* Code practices and recommendations 
+   * scoped private javascript objects using com. entity . module  thanks to Brin King for feedback
+   * awaits sessionstore ready events before it presents the welcome page 
+*/
+
+if(!com) var com={};
+if(!com.taboca) com.taboca={};
+if(!com.taboca.abouttimao) com.taboca.abouttimao={};
+
+addEventListener("load", function () { com.taboca.abouttimao.appStartup() } , false);
+
+
+com.taboca.abouttimao = { 
+
+
+	validLocales : "en-US,en-GB,es-ES,es-AR,pt-PT,pt-BR,it-IT,fr-FR,de-DE" ,
+
+	////
+	/// services
+	//
+	obsSvc: Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService),
+	prefService: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch),
+	searchService: Components.classes["@mozilla.org/browser/search-service;1"].getService(Components.interfaces.nsIBrowserSearchService),
+ 	stringsBundle: null, 
+
+	////
+	///	
+	//
+ 	timaoTheme_timaoTab: null, 
+
+
+	////
+	///  This is used by the about:timao 'page.html' chrome-level web page.
+	//
+	export_getString: function ( propertyId ) {
+                try {
+                        return this.stringsBundle.getString( propertyId );
+                } catch(i) { alert(i) }
+	},
+
+
+	export_launchDonationsPage: function () { 
+
+		var homeURI = this.makeURI("https://addons.mozilla.org/firefox/addon/10284/developers");
+                try {
+                        testHomeTab = Application.activeWindow.open( homeURI );
+                        testHomeTab.focus();
+                } catch (i) {
+                }
+
+	},
+
+	////
+	///
+	//
+	export_launchInstalledHomePage: function () { 
+                var currentHome = this.prefService.getCharPref("browser.startup.homepage");
+		var homeURI = this.makeURI(currentHome);
+                try {
+                        testHomeTab = Application.activeWindow.open( homeURI );
+                        testHomeTab.focus();
+                } catch (i) {
+                }
+	},
+	
+	export_revertHomePage: function () { 
+		var backupHome = this.prefService.getCharPref("extensions.abouttimao.homebackup");
+		var currentHomeInstall = this.prefService.getBoolPref("extensions.abouttimao.homeinstalled");
+                if( currentHomeInstall) {
+                        this.prefService.setBoolPref("extensions.abouttimao.homeinstalled",false);
+                        this.prefService.setCharPref("browser.startup.homepage", backupHome);
+                }
+	},
+	export_getBackupHome: function () { 
+		var backupHome = this.prefService.getCharPref("extensions.abouttimao.homebackup");
+		return backupHome;
+	},
+	export_getCurrentHome: function () { 
+		var currentHome = this.prefService.getCharPref("browser.startup.homepage");
+		return currentHome;
+	},
+
+	////
+	///
+	//
+	export_isAboutTimaoInstalledSearch: function () { 
+
+		var addedEngine = this.searchService.getEngineByName( "Timao Search" );
+		if(addedEngine) { 
+			return true; 
+		} else { 
+			return false; 
+		} 
+
+	},
+	export_isAboutTimaoInstalledHome: function () { 
+                var currentHomeInstall = this.prefService.getBoolPref("extensions.abouttimao.homeinstalled");
+		return currentHomeInstall;
+	},
+
+	////
+	///
+	//
+	export_installHome: function () { 
+                var currentHomeSettings = this.prefService.getCharPref("browser.startup.homepage");
+                var currentHomeInstall = this.prefService.getBoolPref("extensions.abouttimao.homeinstalled");
+		if( ! currentHomeInstall) { 
+			this.prefService.setBoolPref("extensions.abouttimao.homeinstalled",true);
+			this.prefService.setCharPref("extensions.abouttimao.homebackup", currentHomeSettings);
+		} 		
+		var currLocale = "pt-BR";
+		var userLocale = this.export_getLocale();
+		if(this.validLocales.indexOf(userLocale)>-1) {
+			currLocale = userLocale;
+		}
+		this.prefService.setCharPref("browser.startup.homepage", "http://times.taboca.com/co/timao/s-"+currLocale+"/home.html");
+	}, 
+
+	////
+	/// Used by the about:timao ./page.html chrome-level web page
+	//
+
+	export_installSearch: function () { 
+		this.timaoSearchAddSearch();
+ 	}, 
+
+	////
+	///
+	//
+	export_getLocale: function () { 
+		
+		var currentLocale = this.prefService.getCharPref("general.useragent.locale");
+		return currentLocale;
+	}, 
+
+	////
+	///	
+	//
+	dumpConsole: function(aMessage) {
+	 try {
+	   var psvc = Components.classes["@mozilla.org/preferences-service;1"]
+	                        .getService(Components.interfaces.nsIPrefBranch);
+	   var cs = Components.classes["@mozilla.org/consoleservice;1"]
+	                        .getService(Components.interfaces.nsIConsoleService);
+	   cs.logStringMessage("Abouttimao: " + aMessage);
+	 } catch (i) {}
+	},
+
+	////
+	///	
+	//
+ 	makeURI: function (stringIn) { 
+      		var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+		return ios.newURI(stringIn, null, null);
+ 	}, 
+
+
+        getHex: function convert (d) {return d.toString(16);},
+
+        runCosmetics: function () {
+
+		/* This applies to Mac OSX - but at some point this may work 
+		   with other OSes too, so we just set the attribute in there
+                   for now, it should not hurt Linux and Mac OSX */
+
+                var rrHex = this.getHex(250);
+                var ggHex = this.getHex(165);
+                var bbHex = this.getHex(240);
+                var va = "#"+rrHex+ggHex+bbHex;
+                this.changeTitleBar( va, va, va);
+                this.stringsBundle    = document.getElementById("abouttimao_stringsbundle");
+        },
+
+        changeTitleBar: function (gg, ii, aa) {
+                this._header = document.getElementById("main-window");
+                 document.documentElement.setAttribute("titlebarcolor", gg);
+                 document.documentElement.setAttribute("activetitlebarcolor", aa);
+                 document.documentElement.setAttribute("inactivetitlebarcolor", ii);
+        },
+
+
+	////
+	///	
+	//
+ 	checkFirstTime: function () { 
+
+
+		this.runCosmetics();
+		try {
+			var install = this.prefService.getBoolPref("extensions.abouttimao.installed");
+			if(!install) {
+				this.prefService.setBoolPref("extensions.abouttimao.installed",true);
+				this.prefService.setCharPref("general.skins.selectedSkin","classic/1.0");
+				this.prefService.setCharPref("extensions.abouttimao.background","chrome://abouttimao/skin/suggestion-1.jpg");
+				this.timaoSearchPrepareToLaunch();
+				//this.timaoSearchInstallSearch();
+			}
+		}
+		catch(e) { this.dumpConsole(e) }
+	}, 
+
+	////
+	///	
+	//
+	timaoSearchPrepareToLaunch: function () {
+		sessionRestoreObserve =  {
+			observe: function(subject, topic, data) {
+			try {  com.taboca.abouttimao.timaoSearchTryLaunchAboutTimao() } 
+			catch(e) { this.dumpConsole(" session store done failed? " )}
+			} 
+		};
+
+		this.obsSvc.addObserver( sessionRestoreObserve,  "sessionstore-windows-restored" , false);
+	},
+
+	////
+	///	
+	//
+	timaoSearchTryLaunchAboutTimao: function () { 
+		var aboutURI = this.makeURI("about:timao?welcome"); 
+		try { 
+			this.timaoTheme_timaoTab = Application.activeWindow.open( aboutURI );
+			this.timaoTheme_timaoTab.focus();
+		} catch (i) { 
+			this.dumpConsole("not possible yet" );
+	 		setTimeout("com.taboca.abouttimao.timaoSearchTryLaunchAboutTimao()",2000);	
+		} 
+	},
+
+	appStartup: function () { 
+                this.stringsBundle = document.getElementById("abouttimao_stringsbundle");
+       	 	this.checkFirstTime();
+	},
+
+
+
+	////
+	///
+	//
+	timaoSearchAddSearch:function() {
+
+		var currLocale = "en-US";
+		var userLocale = this.export_getLocale();
+		if(this.validLocales.indexOf(userLocale)>-1) { 
+			currLocale = userLocale; 
+		} else { 
+			// default is the above.. "en-US"
+		}  
+	 		
+		try {
+
+                	this.prefService.setBoolPref( "extensions.abouttimao.searchinstalled", true);
+
+			this.searchService.addEngineWithDetails( "TimaoTheme Search", "http://www.timaotheme.com/p/start/"+ currLocale +"/icon.png", "","", "GET", "http://www.timaotheme.com/p/start/"+ currLocale  +"/link.html");
+		
+		} catch(e) {
+			this.dumpConsole(e);
+		}
+		var addedEngine = this.searchService.getEngineByName( "TimaoTheme Search" );
+		addedEngine.addParam("q", "{searchTerms}", null);
+		try {
+			var origEngineObj = addedEngine.wrappedJSObject;
+			origEngineObj._searchForm = "http://times.taboca.com/co/timao/s-"+currLocale+"/link.html";
+			origEngineObj._queryCharset = "UTF-8";
+			origEngineObj._serializeToFile();
+		} catch (i) { dumpConsole(e) } 
+		this.searchService.currentEngine = addedEngine;
+	}
+} 
+
---
+++ products/abouttimao/src-backup/chrome/content/script/page.js
@@ -0,0 +1,181 @@
+$(document).ready(function() {
+	appStart();
+});
+
+
+var command_location ="";
+
+function appStart() { 
+
+	stringsApply();
+
+	try {
+            var probeLocationString = document.location.toString();
+            if(probeLocationString.indexOf("?")>-1) {
+                command_location=probeLocationString.split("?")[1];
+            } else {
+                command_location="settings"; // this is the default page         
+            }
+	} catch(i) {
+		command_location="settings";
+	}
+
+	components_selector_binding();
+
+	if(command_location.indexOf("welcome")>-1)  {
+		runEvents( $("#p_welcome"), "welcome");
+	//	runEvents( $("#navigation_search"), "togglePanel");
+	} 
+
+
+	refreshHome();
+	refreshSearch();
+
+} 
+
+
+function refreshSearch() { 
+	if(app_isSearchInstalled()) { 
+
+                $("#box_searchnotinstalled").css("display","none");
+                $("#box_searchinstalled").css("display","block");
+
+	} else { 
+
+                $("#box_searchnotinstalled").css("display","block");
+                $("#box_searchinstalled").css("display","none");
+
+	} 
+} 
+function refreshHome() { 
+
+   if(app_isHomeInstalled()) {
+                $("#button_recoverhome").css("display","block");
+                $("#button_recoverhome").click( function () {
+                        app_revertHome();
+                        refreshHome();
+                } );
+                $("#button_testhome").click( function () {
+                        app_launchTestHome();
+                } );
+                $("#button_installhome").css("display","none");
+
+                $("#home_detail").css("display","block");
+                document.getElementById("home_detailcurrent").value =  app_getCurrentHome();
+   } else { 
+
+                $("#home_detail").css("display","none");
+                $("#button_installhome").css("display","block");
+		$("#button_recoverhome").click( function () {
+                } );
+                $("#button_testhome").click( function () {
+                } );
+
+   } 
+
+} 
+
+
+/* this is sort of the early stage to the declaratie approach that allows 
+   events, bindings, markup transformations ( ala trmplate style ) and events based
+   transformations in runtme with declarative annotations */
+
+function components_selector_binding() { 
+
+
+	/* We can move all this to be events-based. So then if you need to kick start a bunch of things
+ 	in startup time, yu just kick the events */
+
+	components_apply(".panel", class_components_hide);
+	components_apply("#p_welcome", class_components_hide);
+	components_apply("#box_searchinstalled", class_components_hide);
+
+	events_apply( "welcome", class_components_enablePanel, "id" ); 
+	events_apply( "togglePanel", class_components_togglePanel, "id" ); 
+
+	// we need to move this to state-machine based. Notice that sometimes we want to toggle a panel with state A 
+	// some other itmes with some other state...  So the ability to change event states is very important.
+
+	components_apply("div.button.navigation", class_components_hoverEntry, "togglePanel" );
+
+	components_apply("#button_activate", class_components_hoverEntry, null);
+	components_apply("#button_activate", class_components_clickInner, { callback: app_installSearch } ) ;
+
+	components_apply("#button_donate", class_components_hoverEntry, null);
+	components_apply("#button_donate", class_components_clickInner, { callback: app_linkToDonate } ) ;
+
+	components_apply("#button_installhome", class_components_hoverEntry, null);
+	components_apply("#button_installhome", class_components_clickInner, { callback: function () { app_installHome(); refreshHome()}  } ) ;
+
+} 
+
+function components_apply(a,b,c) { 
+	b($(a), c);	
+}
+
+
+// Synchronous
+function addEvent(evtObj, typeEvent) { 
+	runEvents(evtObj, typeEvent);
+} 
+
+var eventsHash = new Array();
+
+function events_apply( eventType, functionCallback, param) { 
+
+	// need to fix this to support hash of hash?
+	eventsHash[eventType] = { callback: functionCallback, param:param } 
+	
+} 
+
+function runEvents( evtObj, typeEvent )  { 
+	eventsHash[typeEvent].callback( evtObj.attr( eventsHash[typeEvent].param ) );	
+} 
+
+
+function class_components_clickInner ( currObj, followObj ) { 
+  currObj.click(function() {
+
+	if(followObj) { 
+		followObj.callback();
+	} 
+   });
+
+	
+}  
+
+function class_components_enablePanel ( id ) { 
+	$("#"+id).css("display","block");
+} 
+
+function class_components_togglePanel ( id ) { 
+	var targetElement = id.split("-")[1];
+	$(".panel").css("display","none");
+	$("#panel-"+targetElement).css("display","block");
+} 
+
+
+function class_components_hide( currObj) { 
+	currObj.css("display","none");
+} 
+
+function class_components_hoverEntry( currObj, event) { 
+
+   currObj.hover(function() {
+                $(this).addClass("button-highlight");
+			if(event) { 
+			addEvent( $(this) , event);
+			}
+                },function(){
+        $(this).removeClass("button-highlight");
+   });
+
+   currObj.click(function() {
+   });
+
+} 
+
+
+
+
+
