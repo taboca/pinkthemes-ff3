5602015c752d18b7ffd4a42d8eff7c0d60d83e41
Marcio Galli <marciogalli@taboca.local>
2009-11-12 20:27:18 -0800

    patch so that UTF values are properly processed when coming from the URL

--- services/p-start/start/_script/1.2.1/link.js
+++ services/p-start/start/_script/1.2.1/link.js
@@ -11,8 +11,7 @@
 
 				queryVal = queryVal.split("&q=")[0];
 
-
-				queryVal = decode(queryVal);
+				queryVal = utf8decode(decode(queryVal));
 
 				document.forms[0].q.value=queryVal;
 				document.forms[0].submit();
@@ -37,4 +36,34 @@ function decode(value) {
 	var encoded = value;
 	return  unescape(encoded.replace(/\+/g,  " "));
 }
+
+function utf8decode(utftext) {  
+         var string = "";  
+         var i = 0;  
+         var c = c1 = c2 = 0;  
+   
+         while ( i < utftext.length ) {  
+   
+             c = utftext.charCodeAt(i);  
+   
+             if (c < 128) {  
+                 string += String.fromCharCode(c);  
+                 i++;  
+             }  
+             else if((c > 191) && (c < 224)) {  
+                 c2 = utftext.charCodeAt(i+1);  
+                 string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));  
+                 i += 2;  
+             }  
+             else {  
+                 c2 = utftext.charCodeAt(i+1);  
+                 c3 = utftext.charCodeAt(i+2);  
+                 string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));  
+                 i += 3;  
+             }  
+   
+         }  
+   
+         return string;  
+     }  
 
